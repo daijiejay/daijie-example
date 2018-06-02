01428cb64bf482b16795875b5b6a2a97c09cbd76
daijie <daijie@caihangjia.com>
2018-06-02 18:39:03 +0800

    解决guava包版本兼容性问题

--- daijie-example-elasticsearch-cloud/src/main/java/org/daijie/elasticsearch/cloud/dao/BlogSearchRepository.java
+++ daijie-example-elasticsearch-cloud/src/main/java/org/daijie/elasticsearch/cloud/dao/BlogSearchRepository.java
@@ -2,7 +2,9 @@ package org.daijie.elasticsearch.cloud.dao;
 
 import org.daijie.api.model.Blog;
 import org.daijie.elasticsearch.cloud.service.base.BaseSearchRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface BlogSearchRepository extends BaseSearchRepository<Blog, Integer>{
 
 }
