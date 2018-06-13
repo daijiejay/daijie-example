91634bc3460f21d431fc641d7d68bb51066c7e96
daijie <daijie@caihangjia.com>
2018-06-13 16:34:25 +0800

    修改项目名

---
+++ daijie-example-shiro-server/src/main/java/org/daijie/shiro/server/TestController.java
@@ -0,0 +1,13 @@
+//package org.daijie.shiro.server;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class TestController {
+//
+//	@RequestMapping(value = "/api/user")
+//	public Object a(){
+//		return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+//	}
+//}
