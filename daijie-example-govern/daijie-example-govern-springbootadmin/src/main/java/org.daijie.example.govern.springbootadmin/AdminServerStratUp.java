9569ab7164e10e4b10611da07797d04634d2325a
daijie_jay@126.com <daijie_jay@126.com>
2019-04-18 18:39:22 +0800

    2.0項目重构

--- daijie-example-govern/daijie-example-govern-springbootadmin/src/main/java/org.daijie.example.govern.springbootadmin/AdminServerStratUp.java
+++ daijie-example-govern/daijie-example-govern-springbootadmin/src/main/java/org.daijie.example.govern.springbootadmin/AdminServerStratUp.java
@@ -1,18 +1,15 @@
 package org.daijie.example.govern.springbootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
-
-import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
-@EnableTurbine
 @EnableAdminServer
 @EnableEurekaClient
-@EnableDiscoveryClient  
+@EnableDiscoveryClient
 public class AdminServerStratUp {
 
 	public static void main(String[] args) {
